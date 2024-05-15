18ac8ef86d1cee0743d21f52992fa769a9dcf859
dinukasw <dinukasanthush50@gmail.com>
2024-05-15 22:09:33 +0530

    updated

--- src/utils/reportEngine.ts
+++ src/utils/reportEngine.ts
@@ -9,7 +9,10 @@ export const compileReport = async (templateName : string , data : any) => {
     const filePath = path.join(TEMPLATE_DIR, templateName);
     const html = await fs.readFile(filePath, 'utf-8');
     const logoBase64 = base64Encode('./src/public/lh.jpg');
-    return handlebars.compile(html.replace('${logoBase64}', logoBase64))(data);
+    return handlebars.compile(html.replace('${logoBase64}', logoBase64))(data.map((item : any) => {
+        const newItem = {...item,pricePerUnit: parseFloat(item.pricePerUnit).toFixed(2),totalPrice:parseFloat(item.totalPrice).toFixed(2)};
+        return newItem;
+    }));
 }
 function base64Encode(file: fs.PathOrFileDescriptor) {
     try{
